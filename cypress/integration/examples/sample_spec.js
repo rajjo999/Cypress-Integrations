8ff88cc7003da82f47e8298e04b1cce4430a4604
Ranbir Singh <ranbir.singh@c02x5304jgh6.corp.ssense.com>
2019-08-23 14:58:24 -0400

    Added artillery script

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -32,39 +32,39 @@ context('Linked in tests', () => {
 
 
     //
-    // it('go to SSENSE', () => {
-    //
-    //     cy.visit('https://www.ssense.com?fgfgfgfg');
-    //     //cy.clearCookies()
-    //     cy.get('.desktop-search-container > .hidden-tablet-landscape').click()
-    //     //cy.get('.header-search-input').type('gucci{enter}');
-    //     cy.getCookie('guest_checkout_enabled')
-    //         .should('have.property', 'value', 'true')
-    //
-    //     //cy.setCookie('guest_checkout_enabled','false')
-    //
-    // });
-    //
-    it('go to Linkedin', () => {
+    it('go to SSENSE', () => {
 
-        cy.visit('https://linkedin.com/login');
+        cy.visit('https://www.ssense.com?fgfgfgfg');
+        //cy.clearCookies()
+        cy.get('.desktop-search-container > .hidden-tablet-landscape').click()
+        //cy.get('.header-search-input').type('gucci{enter}');
+        cy.getCookie('guest_checkout_enabled')
+            .should('have.property', 'value', 'true')
 
-        cy.get('#username').clear();
-        cy.get('#username').type('ranbir.singh.523@gmail.com');
-        cy.get('#password').clear();
-        cy.get('#password').type('Waheguru@999');
-        cy.get('.btn__primary--large').click();
-        cy.get('.search-global-typeahead__input').type('Smilona bhuyan{enter}')
-        cy.get('.name').click()
-        cy.wait(999);
-        cy.contains('Message').click()
-        for (let i=0;i<15;i++)
-        {
-            cy.get('.msg-form__contenteditable > p').type("Puchi?😋")
-            cy.get('.msg-form__send-button').click()
-        }
+        //cy.setCookie('guest_checkout_enabled','false')
 
     });
+    //
+    // it('go to Linkedin', () => {
+    //
+    //     cy.visit('https://linkedin.com/login');
+    //
+    //     cy.get('#username').clear();
+    //     cy.get('#username').type('ranbir.singh.523@gmail.com');
+    //     cy.get('#password').clear();
+    //     cy.get('#password').type('Waheguru@999');
+    //     cy.get('.btn__primary--large').click();
+    //     cy.get('.search-global-typeahead__input').type('Smilona bhuyan{enter}')
+    //     cy.get('.name').click()
+    //     cy.wait(999);
+    //     cy.contains('Message').click();
+    //     for (let i=0;i<2;i++)
+    //     {
+    //         cy.get('.msg-form__contenteditable > p').type("😋")
+    //         cy.get('.msg-form__send-button').click()
+    //     }
+    //
+    // });
 
 
 });
